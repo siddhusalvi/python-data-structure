c3aee8f545fac5f7e4a1b05baaa1602eb69acbec
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 17:36:32 +0530

    Refactor : final check

--- array/occurance_in_list.py
+++ array/occurance_in_list.py
@@ -2,14 +2,16 @@
 3. Write a Python program to get the number of occurrences of a specified element in an
 array.
 """
-def get_occurance(data ,num): #function to get occurance
+
+
+def get_occurance(list1, number):  # function to get occurance
     count = 0
-    for i in data:
-        if i is num:
+    for i in list1:
+        if i is number:
             count += 1
     return count
-    
 
-data = [1,2,3,4,5,6,7,8,1,2,3,2,1,2,1,21,1]
+
+data = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 2, 1, 2, 1, 21, 1]
 num = 1
-print(get_occurance(data,num))
+print(get_occurance(data, num))
