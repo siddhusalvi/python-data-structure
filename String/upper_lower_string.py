b77426decb7a9f272874baa33c858dcc955ff6b4
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 18:53:10 +0530

    Refactor : final check

--- String/upper_lower_string.py
+++ String/upper_lower_string.py
@@ -7,14 +7,14 @@ def convert_case(word):
     new_word = ""
     new_word2 = ""
     for x in word:
-        if  'a' <= x <= 'z' :
+        if'a' <= x <= 'z':
             new_word += x
         else:
-            new_word +=chr(ord(x) + 32)
-    print("Lower case :",new_word)
+            new_word += chr(ord(x) + 32)
+    print("Lower case :", new_word)
     for x in new_word:
         new_word2 += chr(ord(x)-32)
-    print("Upper case :",new_word2)
+    print("Upper case :", new_word2)
 
 
 word1 = input("enter any word : ")
