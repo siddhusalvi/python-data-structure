ff0f753973c7d664aef40bd2e8fc32a503f693aa
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 18:02:22 +0530

    Refactor : final check

--- list/circularly_identical.py
+++ list/circularly_identical.py
@@ -2,20 +2,22 @@
 14. Write a python program to check whether two lists are circularly identical.
 """
 
-def are_circular_identical(list1,list2):
+
+def are_circular_identical(given_list1, given_list2):
     new_list = []
     for i in range(2):
-        for x in list1:
+        for x in given_list1:
             new_list.append(x)
-    for i in range(0,len(new_list)-len(list2)):
+    for i in range(0, len(new_list) - len(given_list2)):
         counter = 0
-        for j in range(len(list2)):
-            if new_list[i+j] == list2[j]:
+        for j in range(len(given_list2)):
+            if new_list[i + j] == given_list2[j]:
                 counter += 1
-        if(counter == len(list2)):
+        if counter == len(given_list2):
             return True
     return False
 
-list1 = [4,5,1,2,3]
-list2 = [1,2,3,4,5]
-print(are_circular_identical(list1,list2))
+
+list1 = [4, 5, 1, 2, 3]
+list2 = [1, 2, 3, 4, 5]
+print(are_circular_identical(list1, list2))
