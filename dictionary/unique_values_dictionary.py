81cebd5fed549fc7e715dcc4cca0623046d651f4
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 17:58:43 +0530

    Refactor : final check

--- dictionary/unique_values_dictionary.py
+++ dictionary/unique_values_dictionary.py
@@ -5,14 +5,15 @@ Sample Data : [{"V":"S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"},
 Expected Output : Unique Values: {'S005', 'S002', 'S007', 'S001', 'S009'}
 """
 
-def print_unique_value(dict): #funtion tu print unique value
-    list =[]
-    for x,y in dict.items():
-        if y not in list:
-            list.append(y)
-    print(list)
+
+def print_unique_value(given_dict):  # funtion tu print unique value
+    list1 = []
+    for x, y in dict1.items():
+        if y not in list1:
+            list1.append(y)
+    print(list1)
 
 
-dict ={"V":"S001", "VI": "S002", "VIII": "S001", "IVI": "S005","IIVII":"S005", "IIIVIII":"S009","IIVIII":"S007"}
-print(dict)
-print_unique_value(dict)
+dict1 = {"V": "S001", "VI": "S002", "VIII": "S001", "IVI": "S005", "IIVII": "S005", "IIIVIII": "S009", "IIVIII": "S007"}
+print(dict1)
+print_unique_value(dict1)
