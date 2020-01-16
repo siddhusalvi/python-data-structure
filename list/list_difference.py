b3a5bf67687488570a0499e060363ea1f908e53f
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 18:08:56 +0530

    Refactor : final check

--- list/list_difference.py
+++ list/list_difference.py
@@ -1,12 +1,16 @@
 """
-	12. Write a Python program to get the difference between the two lists.
+12. Write a Python program to get the difference between the two lists.
 """
-def list_difference(list1,list2):
+
+
+def list_difference(list1, list2):
     new_list = []
     for x in list1:
         if x not in list2:
             new_list.append(x)
     return new_list
-li1 = [1,2,3,4,5,6,7,8]
-li2 = [1,2,3,4]
-print(list_difference(li1,li2))
+
+
+li1 = [1, 2, 3, 4, 5, 6, 7, 8]
+li2 = [1, 2, 3, 4]
+print(list_difference(li1, li2))
