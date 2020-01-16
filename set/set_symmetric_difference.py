6968b466d1f61a41e6c894e79703d49965dcd0cb
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 18:33:18 +0530

    Refactor : final check

--- set/set_symmetric_difference.py
+++ set/set_symmetric_difference.py
@@ -1,11 +1,14 @@
 """
-	9. Write a Python program to create a symmetric difference.
+9. Write a Python program to create a symmetric difference.
 """
-def set_symmetric_difference(set1,set2):
-    set3 = set1-set2
-    set4 = set2-set1
+
+
+def set_symmetric_difference(set1, set2):
+    set3 = set1 - set2
+    set4 = set2 - set1
     return set3.union(set4)
 
-set1 = {2, 4, 5, 6, 11, 23, 45, 44, 31, 32, 33}
-set2 = {4, 6, 7, 8, 15, 48, 67, 84, 31, 32, 33}
-print(set_symmetric_difference(set1,set2))
+
+set_1 = {2, 4, 5, 6, 11, 23, 45, 44, 31, 32, 33}
+set_2 = {4, 6, 7, 8, 15, 48, 67, 84, 31, 32, 33}
+print(set_symmetric_difference(set_1, set_2))
