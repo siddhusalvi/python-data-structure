88ddf1200f7c8767655018d7acabe1e0bfc616e9
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 18:22:24 +0530

    Refactor : final check

--- list/sum_of_listitems.py
+++ list/sum_of_listitems.py
@@ -1,11 +1,14 @@
 """
 1. Write a Python program to sum all the items in a list.
 """
-def sum_of_listItems(list):#fuction to return sum of list
-    sum = 0
-    for x in list:
-        sum += x
-    return sum
 
-list = [1,2,3,4,5,6,7,8,9,10]
-print(sum_of_listItems(list))
+
+def total_of_list(given_list):  # fuction to return sum of list
+    total = 0
+    for x in given_list:
+        total += x
+    return total
+
+
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(total_of_list(list1))
