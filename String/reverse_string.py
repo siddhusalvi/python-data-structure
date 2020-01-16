c67221fde0a333fffb666ecc1ac47511e2741d67
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 18:44:31 +0530

    Refactor : final check

--- String/reverse_string.py
+++ String/reverse_string.py
@@ -1,10 +1,14 @@
 """
 11. Write a Python program to reverse a string.
 """
-def reverse_string(str):
+
+
+def reverse_string(given_word):
     reverse = ""
-    for x in str:
-        reverse = x +reverse
+    for x in given_word:
+        reverse = x + reverse
     return reverse
+
+
 word = "machinelearning"
 print(reverse_string(word))
