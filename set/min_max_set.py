d1ed8ee3ccd7ce7e72219241a7ff4fb9fcfd44c8
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 18:28:28 +0530

    Refactor : final check

--- set/min_max_set.py
+++ set/min_max_set.py
@@ -1,26 +1,30 @@
 """
-	12. Write a Python program to find maximum and the minimum value in a set.
+12. Write a Python program to find maximum and the minimum value in a set.
 """
-def set_min(set): #function that returns min value in set
-    flag = True
-    for x in set:
-        if flag:
-            min = x
-            flag = False
-        elif min>x:
-            min = x
-    return min
 
-def set_max(set): #function that returns max value in set
-    flag = True
-    for x in set:
-        if flag:
-            max = x
-            flag = False
-        elif max < x:
-            max = x
-    return max
 
-set = {1,2,3,4,5,6,7,8,9}
-print(set_min(set))
-print(set_max(set))
+def set_min(given_set):  # function that returns min value in set
+    flag = True
+    for x in given_set:
+        if flag:
+            minimum = x
+            flag = False
+        elif minimum > x:
+            minimum = x
+    return minimum
+
+
+def set_max(given_set):  # function that returns max value in set
+    flag = True
+    for x in given_set:
+        if flag:
+            maximum = x
+            flag = False
+        elif maximum < x:
+            maximum = x
+    return maximum
+
+
+set1 = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+print(set_min(set1))
+print(set_max(set1))
