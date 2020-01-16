a99636bb42830cf60beeb894462e526e2ab7efed
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 18:18:11 +0530

    Refactor : final check

--- list/sort_by_first.py
+++ list/sort_by_first.py
@@ -1,16 +1,19 @@
 """
 16. Write a Python program to split a list based on first character of word.
 """
-def list_by_frist_chara(list):
+
+
+def list_by_chr(given_list):
     new_list = []
-    for x in list:
+    for x in given_list:
         if x[0] not in new_list:
             new_list.append(x[0])
-            for y in list:
+            for y in given_list:
                 if x[0] == y[0]:
                     new_list.append(y)
     return new_list
 
 
-words = ['be','have','do','say','get','make','go','know','take','see','come','think','look','want','give','use','find','tell','ask','work','seem','feel','leave','call']
-print(list_by_frist_chara(words))
+words = ['be', 'have', 'do', 'say', 'get', 'make', 'go', 'know', 'take', 'see', 'come', 'think', 'look', 'want', 'give',
+         'use', 'find', 'tell', 'ask', 'work', 'seem', 'feel', 'leave', 'call']
+print(list_by_chr(words))
