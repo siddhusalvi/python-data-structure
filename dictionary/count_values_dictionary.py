b3f52d3ddca1bb5e4d5b6ed1885705e198f630c3
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 17:44:00 +0530

    Refactor : final check

--- dictionary/count_values_dictionary.py
+++ dictionary/count_values_dictionary.py
@@ -5,13 +5,17 @@ Sample data: = [{'id': 1, 'success': True, 'name': 'Lary'}, {'id': 2, 'success':
 False, 'name': 'Rabi'}, {'id': 3, 'success': True, 'name': 'Alex'}]
 Expected result: Count of how many dictionaries have success as True
 """
-def get_true_count(list):
+
+
+def get_true_count(list1):
     count = 0
-    for i in list:
-        for x,y in i.items():
-            if (x == "success" and y):
+    for i in list1:
+        for x, y in i.items():
+            if x == "success" and y:
                 count += 1
     return count
-data= [{'id': 1, 'success': True, 'name': 'Lary'}, {'id': 2, 'success':False, 'name': 'Rabi'}, {'id': 3, 'success': True, 'name': 'Alex'}]
-print(get_true_count(data))
 
+
+data = [{'id': 1, 'success': True, 'name': 'Lary'}, {'id': 2, 'success': False, 'name': 'Rabi'},
+        {'id': 3, 'success': True, 'name': 'Alex'}]
+print(get_true_count(data))
