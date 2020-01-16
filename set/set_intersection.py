85c4ac065a7f496f92521e864168a6f73c60858c
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 18:32:10 +0530

    Refactor : final check

--- set/set_intersection.py
+++ set/set_intersection.py
@@ -1,13 +1,16 @@
 """
 6. Write a Python program to create an intersection of sets.
 """
-def set_intersection(set1,set2):
+
+
+def set_intersection(set1, set2):
     new_set = set()
     for x in set1:
         if x in set2:
             new_set.add(x)
     return new_set
 
-set1 = {2, 4, 5, 6, 11, 23, 45, 44, 31, 32, 33}
-set2 = {4, 6, 7, 8, 15, 48, 67, 84, 31, 32, 33}
-print(set_intersection(set1,set2))
+
+set_1 = {2, 4, 5, 6, 11, 23, 45, 44, 31, 32, 33}
+set_2 = {4, 6, 7, 8, 15, 48, 67, 84, 31, 32, 33}
+print(set_intersection(set_1, set_2))
