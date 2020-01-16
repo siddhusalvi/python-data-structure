effb65fd3ef5e18f65bf7eceba295e6a7e2ea7d8
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 18:20:11 +0530

    Refactor : final check

--- list/sort_list_by_tuple.py
+++ list/sort_list_by_tuple.py
@@ -1,28 +1,26 @@
 """
 5. Write a Python program to get a list, sorted in increasing order by the last element in each tuple from a given list of non-empty tuples.
-	Sample List : [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
-	Expected Result : [(2, 1), (1, 2), (2, 3), (4, 4), (2, 5)]
+Sample List : [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+Expected Result : [(2, 1), (1, 2), (2, 3), (4, 4), (2, 5)]
 """
-def sorted_by_last(list):
+
+
+def sorted_by_last(given_list):
     new_list = []
-    while(len(list)!=0):
+    while len(given_list) != 0:
         flag = True
-        for x in list:
+        for x in given_list:
             if flag:
-                min = x[-1]
-                tuple = x
+                minimum = x[-1]
+                tuple1 = x
                 flag = False
-            elif min > x[-1]:
-                min=x[-1]
-                tuple = x
-        new_list.append(tuple)
-        list.remove(tuple)
-    return (new_list)
+            elif minimum > x[-1]:
+                minimum = x[-1]
+                tuple1 = x
+        new_list.append(tuple1)
+        given_list.remove(tuple1)
+    return (*new_list,)
 
 
-
-list = [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
-print(sorted_by_last(list))
-
-
-
+list1 = [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+print(sorted_by_last(list1))
