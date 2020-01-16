4a40c05866aa13f6a1e57bb773886ce8cab9332a
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 18:43:05 +0530

    Refactor : final check

--- String/lower_case.py
+++ String/lower_case.py
@@ -1,16 +1,19 @@
 """
 12. Write a Python program to lowercase first n characters in a string.
 """
-def lower_case_n(str,num):
+
+
+def lower_case_n(given_word, number):
     count = 0
     new_str = ""
-    for x in str:
-        if count < num and 'A' <= x <= 'Z':
+    for x in given_word:
+        if count < number and 'A' <= x <= 'Z':
             new_str += chr(ord(x)+32)
         else:
             new_str += x
         count += 1
     print(new_str)
+
 
 word = "eXTERNALLY"
 num = 3
