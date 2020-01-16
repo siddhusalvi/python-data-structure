e960d9b53812b1b1f9ad7a9d1bd4ccc5063d8231
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 17:37:55 +0530

    Refactor : final check

--- array/reverse_list.py
+++ array/reverse_list.py
@@ -2,10 +2,13 @@
 Write a Python program to reverse the order of the items in the array.
 """
 
-def reverse_list(data):
+
+def reverse_list(given_list):
     new_list = []
-    for i in range(len(data),0,-1):
+    for i in range(len(given_list), 0, -1):
         new_list.append(i)
     return new_list
-list =[1,2,3,4,5,6,7,8,9,10]
-print(reverse_list(list))
+
+
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(reverse_list(list1))
