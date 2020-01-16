8e78ea1c0ff0c825d449334959edf500b29e39c3
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 19:00:28 +0530

    Refactor : final check

--- tuple/reverse_tuple.py
+++ tuple/reverse_tuple.py
@@ -1,11 +1,14 @@
 """
 10. Write a Python program to reverse a tuple.
 """
-def reverse_tuple(tuple1):
+
+
+def reverse_tuple(given_tuple):
     new_tup = ()
-    for k in reversed(tuple1):
+    for k in reversed(given_tuple):
         new_tup = new_tup + (k,)
     return (new_tup)
 
-tuple = (1,2,3,4,5,6,7,8,9,10)
-print(reverse_tuple(tuple))
+
+tuple1 = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+print(reverse_tuple(tuple1))
