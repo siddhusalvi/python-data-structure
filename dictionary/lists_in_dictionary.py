a20c125c6fcdddfb71aafbcc61ef7002becb26cb
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 17:51:26 +0530

    Refactor : final check

--- dictionary/lists_in_dictionary.py
+++ dictionary/lists_in_dictionary.py
@@ -1,11 +1,15 @@
 """
 13. Write a Python program to count number of items in a dictionary value that is a list.
 """
-def count_list(dict):
+
+
+def count_list(dict1):
     count = 0
-    for i,j in dict.items():
-        if isinstance(j,list):
+    for i, j in dict1.items():
+        if isinstance(j, list):
             count += 1
     return count
-d = {'A': [1, 2, 3, 4, 5, 6, 7, 8, 9],'B': 34,'C': 12,'D': [7, 8, 9, 6, 4]}
+
+
+d = {'A': [1, 2, 3, 4, 5, 6, 7, 8, 9], 'B': 34, 'C': 12, 'D': [7, 8, 9, 6, 4]}
 print(count_list(d))
