81598a74c37221139def65774cd7b9eb877bae2b
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 17:46:31 +0530

    Refactor : final check

--- dictionary/dictionary_loop.py
+++ dictionary/dictionary_loop.py
@@ -1,6 +1,6 @@
 """
 4. Write a Python program to iterate over dictionaries using for loops.
 """
-dict = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
-for i,j in dict.items():
-    print(i,j)
+dict1 = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+for i, j in dict1.items():
+    print(i, j)
