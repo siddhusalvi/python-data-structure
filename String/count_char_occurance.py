3e235b6af7300cd9ff80d1fc23303f1a4699ed79
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 18:39:16 +0530

    Refactor : final check

--- String/count_char_occurance.py
+++ String/count_char_occurance.py
@@ -1,18 +1,21 @@
 """
 2. Write a Python program to count the number of characters (character frequency) in a string.
-	Sample String : google.com
-	Expected Result : {'o': 3, 'g': 2, '.': 1, 'e': 1, 'l': 1, 'm': 1, 'c': 1}
+Sample String : google.com
+Expected Result : {'o': 3, 'g': 2, '.': 1, 'e': 1, 'l': 1, 'm': 1, 'c': 1}
 """
+
+
 def count_char_occurance(word):
-    dict = {}
+    dict1 = {}
     for x in word:
         count = 0
         for y in word:
-            if x == y :
-                count +=1
-        record = {x:count}
-        dict.update(record)
-    return dict
+            if x == y:
+                count += 1
+        record = {x: count}
+        dict1.update(record)
+    return dict1
+
 
 word = "google.com"
 print(count_char_occurance(word))
