b253b43e064f1cef1fe0ce5277fba306ce5a32a3
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 18:10:10 +0530

    Refactor : final check

--- list/merge_list.py
+++ list/merge_list.py
@@ -1,11 +1,14 @@
 """
 13. Write a Python program to append a list to the second list.
 """
-def merge_list(li2,li1):#function to merge two lists
-    for x in li1:
-        li2.append(x)
-    return li2
 
-li1 = [1,2,3,4,5,6,7,8]
-li2 = [1,2,3,4]
-print(merge_list(li2,li1))
+
+def merge_list(given_list1, given_list2):  # function to merge two lists
+    for x in given_list2:
+        given_list1.append(x)
+    return given_list1
+
+
+li1 = [1, 2, 3, 4, 5, 6, 7, 8]
+li2 = [1, 2, 3, 4]
+print(merge_list(li2, li1))
