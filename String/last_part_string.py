140c14d3b60c350d8fc39894beb6943c2b861256
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 18:40:51 +0530

    Refactor : final check

--- String/last_part_string.py
+++ String/last_part_string.py
@@ -1,18 +1,21 @@
 """
 8. Write a Python program to get the last part of a string before a specified character.
-	https://www.w3resource.com/python-exercises
-	https://www.w3resource.com/python
+https://www.w3resource.com/python-exercises
+https://www.w3resource.com/python
 """
-def string_before(str,char):
-    if char not in str:
+
+
+def string_before(given_word, c):
+    if c not in given_word:
         return -1
     before_part = ""
-    for x in str:
-        if x == char:
+    for x in given_word:
+        if x == c:
             break
-        before_part +=x
+        before_part += x
     return before_part
+
 
 word = "https://www.w3resource.com/python-exercises"
 char = '-'
-print(string_before(word,char))
+print(string_before(word, char))
