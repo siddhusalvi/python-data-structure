2243d2fff062b958a6f21190865ec02992a4df21
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 17:41:30 +0530

    Refactor : final check

--- dictionary/check_keys_are_exists.py
+++ dictionary/check_keys_are_exists.py
@@ -1,11 +1,14 @@
 """
 12. Write a Python program to check multiple keys exists in a dictionary.
 """
-def check_keys(dict,keys):
-    for x,y in dict.items():
-        if x in keys:
-            print(x,"is present in dictionary")
 
-data = {1: 'a',  2: 'b',  3: 'c', 4: 'd'}
-keys = [1,5,7,3]
-check_keys(data,keys)
+
+def check_keys(given_dict, given_keys):
+    for x, y in given_dict.items():
+        if x in given_keys:
+            print(x, "is present in dictionary")
+
+
+dict1 = {1: 'a', 2: 'b', 3: 'c', 4: 'd'}
+keys = [1, 5, 7, 3]
+check_keys(dict1, keys)
