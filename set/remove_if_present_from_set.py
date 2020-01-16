3b041005dc98617f7b5ecb657584c5151cdc7801
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 18:29:47 +0530

    Refactor : final check

--- set/remove_if_present_from_set.py
+++ set/remove_if_present_from_set.py
@@ -2,12 +2,14 @@
 5. Write a Python program to remove an item from a set if it is present in the set.
 """
 
-def remove_if_present(set,item):
-    if item not in set:
+
+def remove_if_present(given_set, ele):
+    if ele not in given_set:
         return -1
-    set.discard(item)
+    given_set.discard(ele)
     return "item deleted "
+
 
 days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"}
 item = "Monday"
-print(remove_if_present(days,item))
+print(remove_if_present(days, item))
