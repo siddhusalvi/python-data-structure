9ce2cddd60d085d3b62d72cff82209a8e10b85aa
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 18:34:42 +0530

    Refactor : final check

--- set/set_union.py
+++ set/set_union.py
@@ -1,7 +1,9 @@
 """
 7. Write a Python program to create a union of sets.
 """
-def set_union(set1,set2):
+
+
+def set_union(set1, set2):
     new_set = set()
     for x in set1:
         new_set.add(x)
@@ -9,6 +11,8 @@ def set_union(set1,set2):
         if y not in set1:
             new_set.add(y)
     return new_set
-set1 = {2, 4, 5, 6, 11, 23, 45, 44, 31, 32, 33}
-set2 = {4, 6, 7, 8, 15, 48, 67, 84, 31, 32, 33}
-print(set_union(set1,set2))
+
+
+set_1 = {2, 4, 5, 6, 11, 23, 45, 44, 31, 32, 33}
+set_2 = {4, 6, 7, 8, 15, 48, 67, 84, 31, 32, 33}
+print(set_union(set_1, set_2))
