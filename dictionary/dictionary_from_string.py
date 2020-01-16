f113cd5af670ef185e3ea181aeb574ef5d7d6cdd
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 17:45:18 +0530

    Refactor : final check

--- dictionary/dictionary_from_string.py
+++ dictionary/dictionary_from_string.py
@@ -4,16 +4,19 @@ Note: Track the count of the letters from the string.
 Sample string : 'w3resource'
 Expected output: {'3': 1, 's': 1, 'r': 2, 'u': 1, 'w': 1, 'c': 1, 'e': 2, 'o': 1}
 """
-def get_count_of_letters(word):#function that return dictionary from string
-    dict = {}
-    for x in word:
+
+
+def get_count_of_letters(given_word):  # function that return dictionary from string
+    dict1 = {}
+    for x in given_word:
         count = 0
-        for y in word:
-            if x == y :
-                count +=1
-        record = {x:count}
-        dict.update(record)
-    return dict
+        for y in given_word:
+            if x == y:
+                count += 1
+        record = {x: count}
+        dict1.update(record)
+    return dict1
+
 
 word = "siddhesh"
 print(get_count_of_letters(word))
