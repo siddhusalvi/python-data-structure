f3195af05695259336b249afeed66b89e9fd55cc
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 17:39:49 +0530

    Refactor : final check

--- dictionary/add_to_dictionary.py
+++ dictionary/add_to_dictionary.py
@@ -2,10 +2,10 @@
 2. Write a Python script to add a key to a dictionary.
 """
 
-dict = {"a":1,"b":2,"c":3}
-print(dict)
-num =4
-ans ="d"
-record = {num:ans}
-dict.update(record)
-print(dict)
+dict1 = {"a": 1, "b": 2, "c": 3}
+print(dict1)
+number = 4
+ans = "d"
+record = {number: ans}
+dict1.update(record)
+print(dict1)
