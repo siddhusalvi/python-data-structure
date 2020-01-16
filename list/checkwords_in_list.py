1d43ad172936360966158422470cf0b01159dd99
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 18:00:38 +0530

    Refactor : final check

--- list/checkwords_in_list.py
+++ list/checkwords_in_list.py
@@ -2,14 +2,16 @@
 8. Write a Python program to find the list of words that are longer than n from a given list of words.
 """
 
-def check_word_length(list,lenth):
+
+def check_word_length(given_list, given_lenth):
     count = 0
-    for i in list:
-        if len(i)>lenth:
+    for i in given_list:
+        if len(i) > given_lenth:
             count += 1
     return count
 
-Days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"}
-num  = 6
 
-print(check_word_length(Days,num))
+Days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"}
+number = 6
+
+print(check_word_length(Days, number))
