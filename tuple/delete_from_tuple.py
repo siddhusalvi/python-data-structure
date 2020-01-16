8a08ff1b71a59bbc46fe863df2c7b10cfa1fe3b5
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 18:56:58 +0530

    Refactor : final check

--- tuple/delete_from_tuple.py
+++ tuple/delete_from_tuple.py
@@ -1,19 +1,23 @@
 """
 8. Write a Python program to remove an item from a tuple.
 """
-def remove_from_tuple(tuple,ele):
+
+
+def remove_from_tuple(given_tuple, ele):
     new_list = []
-    if ele not in tuple:
+    if ele not in given_tuple:
         return -1
     flag = True
-    for x in tuple:
+    for x in given_tuple:
         if flag and x == ele:
             print(x)
             flag = False
         else:
             new_list.append(x)
     return(*new_list,)
-tuple = "w","r", "s", "o", "u", "r", "c", "e"
+
+
+tuple1 = "w", "r", "s", "o", "u", "r", "c", "e"
 ele = 'e'
-tuple =remove_from_tuple(tuple,ele)
-print(tuple)
+tuple1 = remove_from_tuple(tuple1, ele)
+print(tuple1)
