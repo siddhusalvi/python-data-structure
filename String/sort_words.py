202cbfe3c44a310aaf4891ab0fec579369084949
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 18:46:14 +0530

    Refactor : final check

--- String/sort_words.py
+++ String/sort_words.py
@@ -1,35 +1,40 @@
-7. Write a Python program that accepts a comma separated sequence of words as input and prints the unique words in sorted form (alphanumerically).
-	Sample Words : red, white, black, red, green, black
-	Expected Result : black, green, red, white,red
 """
-def sort_words(str):
+7. Write a Python program that accepts a comma separated sequence of words as input and prints the unique words in sorted form (alphanumerically).
+Sample Words : red, white, black, red, green, black
+Expected Result : black, green, red, white,red
+"""
+
+
+def sort_words(given_list):
     list = []
     new_list = []
     start = 0
     count = 0
-    for i in str:
+    for i in given_list:
         if i == ',':
             #print(str[start:count])
-            temp = str[start:count]
+            temp = given_list[start:count]
             list.append(temp)
             start = count + 1
         count +=1
-    temp = str[start:count]
+    temp = given_list[start:count]
     list.append(temp)
-    while(len(list) != 0):
+    while len(list) != 0:
         flag = True
         for x in list:
             if flag:
-                char = x[0]
+                c = x[0]
                 word = x
                 flag = False
-            elif x < char:
-                char = x[0]
+            elif x < c:
+                c = x[0]
                 word = x
         new_list.append(word)
         #print(word)
         list.remove(word)
     print(new_list)
 
-str = input("enter words by , : ")
-sort_words(str)
+
+words = input("enter words by , : ")
+sort_words(words)
+
