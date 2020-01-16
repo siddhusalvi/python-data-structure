007f783acb658f018c1cd1d04b218b4ed522d1ee
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 18:12:58 +0530

    Refactor : final check

--- list/permute_list.py
+++ list/permute_list.py
@@ -2,15 +2,16 @@
 11. Write a Python program to generate all permutations of a list in Python.
 """
 
-def list_permute(list,start,end):
+
+def list_permute(given_list, start, end):
     if start == end:
-        print(list)
+        print(given_list)
     else:
-        for i in range(start,end+1):
-            list[start] ,list[i] = list[i],list[start]
-            list_permute(list,start+1,end)
-            list[start], list[i] = list[i], list[start]
+        for i in range(start, end + 1):
+            given_list[start], given_list[i] = given_list[i], given_list[start]
+            list_permute(given_list, start + 1, end)
+            given_list[start], given_list[i] = given_list[i], given_list[start]
 
-list = [1,2,3,4]
 
-list_permute(list,0,len(list)-1)
+list1 = [1, 2, 3, 4]
+list_permute(list1, 0, len(list1) - 1)
