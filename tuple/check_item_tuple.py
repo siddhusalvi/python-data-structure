de4bab62d89b724138c33c8214a5fd0bff6b0d5a
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 18:54:47 +0530

    Refactor : final check

--- tuple/check_item_tuple.py
+++ tuple/check_item_tuple.py
@@ -2,15 +2,15 @@
 6. Write a Python program to check whether an element exists within a tuple.
 """
 
-def check_item(tuple,ele):
-    for x in tuple:
+
+def check_item(given_tuple, ele):
+    for x in given_tuple:
         if x == ele:
             return True
     return False
 
-tuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+
+tuple1 = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
 word = "mango"
 
-print(check_item(tuple,word))
-
-
+print(check_item(tuple1, word))
