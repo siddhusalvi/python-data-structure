4ec1c5a3e2a1077ecb7207d88a984088a447028f
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 18:58:50 +0530

    Refactor : final check

--- tuple/repeated_in_tuple.py
+++ tuple/repeated_in_tuple.py
@@ -1,16 +1,20 @@
 """
 5. Write a Python program to find the repeated items of a tuple.
 """
-def find_repeated_items(data):
+
+
+def find_repeated_items(given_tuple):
     new_list = []
-    for x in data:
+    for x in given_tuple:
         if x in new_list:
-            print(x,"is repeated")
+            print(x, "is repeated")
         else:
             new_list.append(x)
     print(new_list)
 
+
 data = (1,2,3,4,5,6,7,8,9,2,3,4,5,5)
 find_repeated_items(data)
+
 
 
