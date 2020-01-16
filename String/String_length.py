de09c3b0886271de2946ef1b674711b624458e17
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 18:47:04 +0530

    Refactor : final check

--- String/String_length.py
+++ String/String_length.py
@@ -1,9 +1,11 @@
 """
-	1. Write a Python program to calculate the length of a string.
+1. Write a Python program to calculate the length of a string.
 """
-def count_length(str):
+
+
+def count_length(given_word):
     count = 0
-    for i in str:
+    for i in given_word:
         count += 1
     return count
 
