b6c55553b8876501e2c328f309ada7807f4d3f0a
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 17:52:56 +0530

    Refactor : final check

--- dictionary/remove_key_dictionary.py
+++ dictionary/remove_key_dictionary.py
@@ -1,15 +1,18 @@
 """
 6. Write a Python program to remove a key from a dictionary.
 """
-def remove_key(dict,key):
-    if key in dict:
-        del dict[key]
+
+
+def remove_key(given_dict, key1):
+    if key1 in given_dict:
+        del given_dict[key1]
         print("key removed current dictionary :c")
-        print(dict)
+        print(given_dict)
     else:
         print("Key not found!")
 
-dict = {'a':1,'b':2,'c':3,'d':4}
-print("Initial dictionary",dict)
-key=input("Enter the key to delete:")
-remove_key(dict,key)
+
+dict1 = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+print("Initial dictionary", dict1)
+key = input("Enter the key to delete:")
+remove_key(dict1, key)
