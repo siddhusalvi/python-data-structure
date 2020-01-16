9ace67d6aa78de87e9ff0ddc102c1c404504b57e
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 17:42:18 +0530

    Refactor : final check

--- dictionary/concatenate_dictionary.py
+++ dictionary/concatenate_dictionary.py
@@ -8,9 +8,9 @@ dic3={5:50,6:60}
 Expected Result : {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
 """
 
-dict1={1:10, 2:20}
-dict2={3:30, 4:40}
-dict3={5:50,6:60}
+dict1={1: 10, 2: 20}
+dict2={3: 30, 4: 40}
+dict3={5: 50, 6: 60}
 dict1.update(dict2)
 dict1.update(dict3)
 print(dict1)
