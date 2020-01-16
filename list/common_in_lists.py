7d6ba797651cc31a44932cad24829a5a59635102
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 18:06:33 +0530

    Refactor : final check

--- list/common_in_lists.py
+++ list/common_in_lists.py
@@ -1,12 +1,15 @@
 """
 9. Write a Python function that takes two lists and returns True if they have at least one common member.
 """
-def check_common_in_list(list1,list2):#fuction return true if atlest one element is common in both list
+
+
+def check_common_in_list(list1, list2):  # function return true if at least one element is common in both list
     for x in list1:
         if x in list2:
             return True
     return False
 
-list1 = [1,2,3,4,5]
-list2 = [6,7,8,9,10]
-print(check_common_in_list(list1,list2))
+
+li1 = [1, 2, 3, 4, 5]
+li2 = [6, 7, 8, 9, 10]
+print(check_common_in_list(li1, li2))
--- list/copy_list.py
+++ list/copy_list.py
@@ -1,11 +1,14 @@
 """
-	7. Write a Python program to clone or copy a list.
+7. Write a Python program to clone or copy a list.
 """
-def clone_list(list):
-    new_list = []
-    for x in list:
+
+
+def clone_list(given_list):
+    new_list =[ ]
+    for x in given_list:
         new_list.append(x)
     return new_list
 
-List = [1,2,3,4,5,6,7,8,1,5,4,8,6,3]
+
+List = [1, 2, 3, 4, 5, 6, 7, 8, 1, 5, 4, 8, 6, 3]
 print(clone_list(List))
