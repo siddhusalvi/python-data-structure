d5ffcac89c068244a1759be81c1a02daf01c7300
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 18:16:44 +0530

    Refactor : final check

--- list/remove_element_025.py
+++ list/remove_element_025.py
@@ -1,18 +1,21 @@
 """
-	10. Write a Python program to print a specified list after removing the 0th, 4th and 5th elements.
-	Sample List : ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
-	Expected Output : ['Green', 'White', 'Black']
+10. Write a Python program to print a specified list after removing the 0th, 4th and 5th elements.
+sample List : ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+Expected Output : ['Green', 'White', 'Black']
 """
-def remove_items(list,indices):
-    if(max(indices)>len(list)-1):
-        return  "invalid input"
+
+
+def remove_items(given_list, indices):
+    if max(indices) > len(given_list) - 1:
+        return "invalid input"
     else:
-        while(len(indices)!=0):
-            maxin = max(indices)
-            list.pop(maxin)
-            indices.remove(maxin)
-        return list
+        while len(indices) != 0:
+            maximum = max(indices)
+            given_list.pop(maximum)
+            indices.remove(maximum)
+        return given_list
+
 
 List = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
-indices = [0,4,5]
-print(remove_items(List,indices))
+indices = [0, 4, 5]
+print(remove_items(List, indices))
