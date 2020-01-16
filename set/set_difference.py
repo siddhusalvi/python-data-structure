f579d106e4a553a53b5e8bdcd7385c5d7808f62d
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 18:30:46 +0530

    Refactor : final check

--- set/set_difference.py
+++ set/set_difference.py
@@ -2,7 +2,8 @@
 8. Write a Python program to create set difference.
 """
 
-def set_difference(set1,set2):
+
+def set_difference(set1, set2):
     new_set = set()
     for x in set1:
         if x not in set2:
@@ -10,7 +11,7 @@ def set_difference(set1,set2):
     return new_set
 
 
-set1 = {2, 4, 5, 6, 11, 23, 45, 44, 31, 32, 33}
-set2 = {4, 6, 7, 8, 15, 48, 67, 84, 31, 32, 33}
+set_1 = {2, 4, 5, 6, 11, 23, 45, 44, 31, 32, 33}
+set_2 = {4, 6, 7, 8, 15, 48, 67, 84, 31, 32, 33}
 
-print(set_difference(set1,set2))
+print(set_difference(set_1, set_2))
