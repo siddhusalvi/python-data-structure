5bf96af577bd03117a0d72fbbbf176c47e1458d1
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 18:03:05 +0530

    Refactor : final check

--- list/common_in_list.py
+++ list/common_in_list.py
@@ -1,15 +1,17 @@
 """
-	15. Write a Python program to find common items from two lists.
+15. Write a Python program to find common items from two lists.
 """
 
-def list_intersection(list1,list2):
+
+def list_intersection(list1, list2):
     new_list = []
     for x in list1:
         if x in list2:
             new_list.append(x)
     return new_list
 
-li1 = [1,2,3,4,5,6,7,8]
-li2 = [1,2,3,4]
 
-print(list_intersection(li1,li2))
+li1 = [1, 2, 3, 4, 5, 6, 7, 8]
+li2 = [1, 2, 3, 4]
+
+print(list_intersection(li1, li2))
