588fdee25aecf2d2f16815c7f19eec413cc0a3cb
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 17:48:11 +0530

    Refactor : final check

--- dictionary/dictionary_of_exponent.py
+++ dictionary/dictionary_of_exponent.py
@@ -5,15 +5,16 @@ Sample Dictionary ( n = 5) :
 Expected Output : {1: 1, 2: 4, 3: 9, 4: 16, 5: 25}
 """
 
-def create_power_dictionary(num):#function to create dictionary of numbers and powers
-    dict = {}
-    for i in range(1,num+1):
-        num = i
-        pow = num*num
-        key ={num:pow}
-        dict.update(key)
-    print(dict)
+
+def create_power_dictionary(number):  # function to create dictionary of numbers and powers
+    dict1 = {}
+    for i in range(1, number + 1):
+        number = i
+        power = number * number
+        key = {number: power}
+        dict1.update(key)
+    print(dict1)
 
 
-num =int(input("Enter a number to create dictionary : "))
+num = int(input("Enter a number to create dictionary : "))
 create_power_dictionary(num)
