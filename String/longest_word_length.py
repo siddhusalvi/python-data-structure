97b6470d63d9582979adc4dae621cfaa565a2ef9
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 18:42:05 +0530

    Refactor : final check

--- String/longest_word_length.py
+++ String/longest_word_length.py
@@ -1,15 +1,18 @@
 """
 5. Write a Python function that takes a list of words and returns the length of the longest one.
 """
-def get_max_len(words):
-    maxlen = 0
-    for word in words:
-        count  = 0
+
+
+def get_max_len(given_words):
+    maximum_len = 0
+    for word in given_words:
+        count = 0
         for letter in word:
             count += 1
-        if count > maxlen:
-            maxlen = count
-    return maxlen
+        if count > maximum_len:
+            maximum_len = count
+    return maximum_len
 
-words = ['a','abc','abcd','abcde','abcdefg','abcdefrg']
+
+words = ['a', 'abc', 'abcd', 'abcde', 'abcdefg', 'abcdefrg']
 print(get_max_len(words))
