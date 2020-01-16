e373de15255d0baf81685a53fcc79f7b923daa71
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 18:49:02 +0530

    Refactor : final check

--- String/substring_occurance.py
+++ String/substring_occurance.py
@@ -1,12 +1,14 @@
 """
 10. Write a Python program to count occurrences of a substring in a string.
 """
-def count_substring(str,substr):
-    if substr not in str:
+
+
+def count_substring(given_str, substr):
+    if substr not in given_str:
         return -1
     count = 0
     new_str = ""
-    for x in str:
+    for x in given_str:
         if substr in new_str:
             count += 1
             new_str = x
