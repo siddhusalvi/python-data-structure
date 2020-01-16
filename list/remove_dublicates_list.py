11212b88bd3dfc6ff8e241f8d5dfd9d25526a519
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 18:14:48 +0530

    Refactor : final check

--- list/remove_dublicates_list.py
+++ list/remove_dublicates_list.py
@@ -1,13 +1,15 @@
 """
 6. Write a Python program to remove duplicates from a list.
 """
-def remove_dublicates(list):#Function to remove dublicates
+
+
+def remove_repeats(given_list):  # Function to remove dublicates
     new_list = []
-    for x in list:
+    for x in given_list:
         if x not in new_list:
             new_list.append(x)
     print(new_list)
 
 
-List = [1,2,3,4,5,6,7,8,1,5,4,8,6,3]
-remove_dublicates(List)
+List = [1, 2, 3, 4, 5, 6, 7, 8, 1, 5, 4, 8, 6, 3]
+remove_repeats(List)
